6ab3fbb0c8d6875f6a7bc397c2d4f65228a1036d
SF <fey@ambihome.com>
2023-03-24 16:02:11 +0100

    .

--- lib/dep.ex
+++ lib/dep.ex
@@ -1,6 +1,7 @@
 defmodule Dep do
   @path :code.priv_dir(:dep) ++ '/test.txt'
   def read_from_priv do
-    {@path, File.read!(@path)}
+    path = :code.priv_dir(:dep) ++ '/test.txt'
+    {@path, path, File.read!(@path)}
   end
 end
